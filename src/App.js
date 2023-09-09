4dca5e2a46281958170b06ddd62e7dfb6c0afa09
Wojciech Tobiś <wojciech.tobis@relativity.com>
2023-09-09 13:41:41 +0200

    Fetch current position

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import React, { useState, useEffect } from 'react'
 import ReactDOMServer from 'react-dom/server';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import { useMap } from 'react-leaflet/hooks'
 import L from 'leaflet';
+
 import './App.css';
 
+function UpdateMapComponent({center}) {
+  const map = useMap()
+  useEffect(() => {
+    console.log('In MyComponent')
+    map.setView(center, 15)
+  }, [center])
+  return null
+}
+
 function App() {
-  const position = [50.01563262939453, 20.024200439453126]
+  const initialPosition = [50.04, 19.96]
+  const [position, setPosition] = useState(initialPosition)
 
   const iconSize = 40
-
   const src = '/marker.svg'
 
   const elem = <div>
@@ -21,9 +34,20 @@ function App() {
     iconAnchor: [iconSize / 2, iconSize / 2],
   })
 
+  useEffect(() => {
+    fetch('https://api.ttss.pl/positions/?type=t')
+      .then((response) => response.json())
+      .then((json) => json.pos['413'])
+      .then(currentPosition => setPosition([currentPosition.lat, currentPosition.lon]))
+      .then(() => console.log('In fetch'))
+  }, [])
+
+  console.log('position:::', position)
+
   return (
     <div className='map-container-wrapper'>
-      <MapContainer center={position} zoom={15} className='map-container'>
+      <MapContainer center={position} zoom={12} className='map-container'>
+        <UpdateMapComponent center={position}/>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
