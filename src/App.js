2ac68cf158824b19b0b6680270eb13c2ddc8f676
Wojciech Tobiś <wojciech.tobis@relativity.com>
2023-09-09 12:38:28 +0200

    Add basic map with static position

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import ReactDOMServer from 'react-dom/server';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
 import './App.css';
 
 function App() {
+  const position = [50.01563262939453, 20.024200439453126]
+
+  const iconSize = 40
+
+  const src = '/marker.svg'
+
+  const elem = <div>
+    <img src={process.env.PUBLIC_URL + src} alt="" style={{width: iconSize}} />
+  </div>
+
+  const icon = new L.divIcon({
+    html: ReactDOMServer.renderToStaticMarkup(elem),
+    className: null,
+    iconSize: [iconSize, iconSize],
+    iconAnchor: [iconSize / 2, iconSize / 2],
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='map-container-wrapper'>
+      <MapContainer center={position} zoom={15} className='map-container'>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position} icon={icon} />
+      </MapContainer>
     </div>
   );
 }
